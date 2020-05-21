3e7142f8bdb88da412e13713786b38ea53ebea07
WEINIEX <550851429@qq.com>
2020-05-21 12:23:11 +0800

    fix:更改测试页面

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react'
-import logo from './images/demopage.png';
+import logo from './images/11111.jpg';
 import './App.css';
 import ReactEcharts from 'echarts-for-react'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sourcedata: [],
+      deviceId: 111,
+      apiRoot: "http://rap2.taobao.org:38080/app/mock/254956/device/deviceInfo/access",//请求Api
+    }
+  }
+  componentDidMount() {
+    //这是一个fetch示例
+    fetch(`${this.state.apiRoot}?deviceId=${this.state.deviceId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8'
+      },
+      mode: 'cors',
+      cache: 'default'
+    })
+      .then(res => res.json())
+      .then((data) => {
+        console.log(data.data)
+        this.setState({
+          sourcedata: data.data
+        })
+      })
+  }
+  //此为echarts参数配置示例
   getOption = () => {
     let option = {
       xAxis: {
-          type: 'category',
-          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
       },
       yAxis: {
-          type: 'value'
+        type: 'value'
       },
       series: [{
-          data: [820, 932, 901, 934, 1290, 1330, 1320],
-          type: 'line'
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line'
       }]
     };
     return option;
@@ -24,24 +51,35 @@ export default class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <img  src={logo} className="Exercise-img" alt="exercise"></img>
-          <ReactEcharts
-              style={{ height: '500px', width: '500px' ,background:'white'}}
-              option={this.getOption()}
-            >
-          </ReactEcharts>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+          <p>题目要求：使用相关技术实现图片示例</p>
+          <img src={logo} className="Exercise-img" alt="exercise"></img>
+          <div>
+            <p>echarts调用示例</p>
+            <p>echarts官网: https://echarts.apache.org/zh/index.html</p>
+            <ReactEcharts
+            style={{ height: '500px', width: '500px', background: 'white' }}
+            option={this.getOption()}
           >
-            Learn React
-          </a>
+          </ReactEcharts>
+          </div>
+          <div>
+            <p>fetch请求数据示例</p>
+            <ul>
+              {this.state.sourcedata.map(item => {
+                return <li key={item.name}>
+                  <span>{item.name} </span>
+                  <span>{item.value} </span>
+                </li>
+              })}
+            </ul>
+          </div>
+          <p>antd官网: https://ant.design/components/button-cn/</p>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
         </header>
       </div>
     );
