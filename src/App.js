6a23541c84b1be60bd003e604e26d0e84067b95f
WEINIEX <550851429@qq.com>
2020-05-21 10:44:39 +0800

    fix: 修改测试界面，增加ecahrts插件

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import logo from './images/demopage.png';
 import './App.css';
+import ReactEcharts from 'echarts-for-react'
 
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  getOption = () => {
+    let option = {
+      xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+          type: 'value'
+      },
+      series: [{
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+          type: 'line'
+      }]
+    };
+    return option;
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <img  src={logo} className="Exercise-img" alt="exercise"></img>
+          <ReactEcharts
+              style={{ height: '500px', width: '500px' ,background:'white'}}
+              option={this.getOption()}
+            >
+          </ReactEcharts>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
